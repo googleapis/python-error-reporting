c1297d10555336cb842d62b2ad8c43cb76006e4c
Anthonios Partheniou <partheniou@google.com>
2023-01-25 11:49:51 -0500

    remove workaround

--- google/cloud/errorreporting_v1beta1/__init__.py
+++ google/cloud/errorreporting_v1beta1/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from google.cloud.errorreporting_v1beta1_v1beta1 import gapic_version as package_version
+from google.cloud.errorreporting_v1beta1 import gapic_version as package_version
 
 __version__ = package_version.__version__
 
